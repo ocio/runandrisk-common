b2a090e1d1eb4a7d3e630659eabcf061b5d256fd
Josema <enzo@josema.es>
2019-05-23 14:54:03 +0200

    Using runandrisk-common

--- const/index.js
+++ const/index.js
@@ -7,3 +7,10 @@ const INSTRUCTION = {
     CONQUEST: 0, // when troops CONQUEST a tile
     ADD: 1 // add units
 }
+
+const GAME_STATUS = {
+    WAITING_PLAYERS: 'WAITING_PLAYERS',
+    PLAYING: 'PLAYING'
+}
+
+module.exports = { TILE, INSTRUCTION, GAME_STATUS }
