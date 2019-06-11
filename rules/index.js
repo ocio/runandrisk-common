b96e4e8597d63e4b045120495cf9c3e349d21f5b
Josema <enzo@josema.es>
2019-06-11 10:08:02 +0200

    Added isAllowedToSendUnits

--- rules/index.js
+++ rules/index.js
@@ -2,4 +2,12 @@ function calcScore({ kills, power, units }) {
     return Math.round(kills * 5 + power * 10 + units)
 }
 
-module.exports = { calcScore }
+function isAllowedToSendUnits({ owner_from, owner_to, player_index }) {
+    return (
+        owner_from === player_index ||
+        owner_to === player_index ||
+        owner_to === undefined
+    )
+}
+
+module.exports = { calcScore, isAllowedToSendUnits }
