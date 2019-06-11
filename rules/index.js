d7acd29dc82be22d65c4340da4ce4fc3783b0b56
Josema <enzo@josema.es>
2019-06-11 13:04:58 +0200

    Added nextRecruitment and stopRecruitment

--- rules/index.js
+++ rules/index.js
@@ -1,3 +1,5 @@
+const { now } = require('../utils')
+
 function calcScore({ kills, power, units }) {
     return Math.round(kills * 5 + power * 10 + units)
 }
@@ -10,4 +12,17 @@ function isAllowedToSendUnits({ owner_from, owner_to, player_index }) {
     )
 }
 
-module.exports = { calcScore, isAllowedToSendUnits }
+function nextRecruitment() {
+    return now() + 3 * 60 // timestamp + minutes * seconds
+}
+
+function stopRecruitment(n) {
+    return n + 30 // seconds
+}
+
+module.exports = {
+    calcScore,
+    isAllowedToSendUnits,
+    nextRecruitment,
+    stopRecruitment
+}
