72a60920573fa6736b653c02420ae611203ed5bb
Josema <enzo@josema.es>
2019-07-17 16:06:20 +0200

    Added calcPercentageConquered

--- rules/index.js
+++ rules/index.js
@@ -1,7 +1,12 @@
+const { TILE } = require('../const')
 // const { now } = require('../utils')
 
 function calcScore({ kills, power, units }) {
-    return Math.round(kills * 5 + power * 10 + units)
+    return Math.round(kills * 1 + power * 5 + units * 2)
+}
+
+function calcPercentageConquered({ conquered, tile_type }) {
+    return conquered + (tile_type === TILE.COTTAGE ? 2 : 1)
 }
 
 function isAllowedToSendUnits({ owner_from, owner_to, player_index }) {
@@ -19,5 +24,6 @@ function calcRecruitment({ power, clicks }) {
 module.exports = {
     calcScore,
     isAllowedToSendUnits,
-    calcRecruitment
+    calcRecruitment,
+    calcPercentageConquered
 }
