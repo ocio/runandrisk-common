fcbe5d0060ed13c73c11b5ec9debec36c6896c05
Josema <enzo@josema.es>
2019-07-19 12:53:32 +0200

    Small change isAllowedToSendUnits

--- rules/index.js
+++ rules/index.js
@@ -11,14 +11,15 @@ function calcPercentageConquered({ conquered, tile_type }) {
 
 function isAllowedToSendUnits({ owner_from, owner_to, player_index }) {
     return (
+        owner_from === undefined ||
+        owner_to === undefined ||
         owner_from === player_index ||
-        owner_to === player_index ||
-        owner_to === undefined
+        owner_to === player_index
     )
 }
 
 function calcRecruitment({ power, clicks }) {
-    return Math.round((power * clicks) / 100)
+    return Math.round((power * clicks) / 200)
 }
 
 module.exports = {
